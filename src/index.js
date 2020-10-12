b747e4f9b69a38e5dbf98f29b6dae3880cb3044d
wunlei <flowersonviolent@gmail.com>
2020-10-13 00:08:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,9 +37,42 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
+var exprReg = /[0-1]{10,10}|[*]*/g;
+var letterReg = /[0-1*]{2,2}/g;
+let wordArray = [];
+
 function decode(expr) {
-    // write your solution here
-}
+    var exprArray = expr.match(exprReg);
+    for (let i = 0; i < exprArray.length - 1; i++) {
+        let letterArray = (exprArray[i]).match(letterReg);
+        if (exprArray[i] === "**********") {
+            let space = " ";
+            wordArray.push(space);
+        } else {
+            let word = "";
+            for (let j = 0; j < letterArray.length; j++) {
+                if (letterArray[j] === "10") {
+                    word = word + ".";
+                } else if (letterArray[j] === "11") {
+                    word = word + "-";
+                } else {
+                    word;
+                }
+            }
+            wordArray.push(word);
+        }
+    }
+    let decodeMorse = "";
+    for (let k = 0; k < wordArray.length; k++) {
+        if (MORSE_TABLE.hasOwnProperty(wordArray[k])) {
+            decodeMorse = decodeMorse + MORSE_TABLE[wordArray[k]];
+        } else {
+            decodeMorse = decodeMorse + " ";
+        }
+    }
+    return decodeMorse;
+};
 
 module.exports = {
     decode
